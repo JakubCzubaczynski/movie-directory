1af4d53cea02807130e42e19dd8020d6bee0540e
JakubCzubaczynski <kuba33211@gmail.com>
2020-12-20 18:40:49 +0100

    update

--- js/app.js
+++ js/app.js
@@ -1,24 +1,19 @@
-
 $(document).ready(function () {
 	$('body').show();
 	pageCount = 1;
-	let configData = null;
 	const key = '25141123a6896a890d381900b61e2af6';
 	let content = [];
 
 
 	//SEARCH FUNCTION
 
-	let runSearch = function (url, y, search) {
+	let runSearch = (url, y, search) => {
 
 		fetch(url)
 			.then((response) => {
 				return response.json();
 			})
 			.then((data) => {
-				console.log(data.results.length);
-				console.log(data);
-
 				for (let i = 0; i < data.results.length; i++) {
 
 					let imagePath = "https://image.tmdb.org/t/p/w185";
@@ -37,7 +32,6 @@ $(document).ready(function () {
 						image: imagePath
 					}
 
-					
 					$('.display').append(`
 				<div class='movie-container col-6 col-sm-4 col-md-3 fade-in' data-id=${content[i].id}>
 					<img height='277' class='mx-auto d-block img-fluid' src='${content[i].image}' onerror="this.onerror=null; this.src='default.png'">
@@ -45,124 +39,108 @@ $(document).ready(function () {
 					<p>${content[i].release}</p>
 				</div>
 				`);
-				
+
 					imagePath = '';
 				}
-				if(search)
-				{
+				if (search) {
 					$('.display').append(`<button class="show-more p-2" type="button" data-search='${search}'><span>Show more</span></button>`);
-				}
-				else if (y === null) {
+				} else if (y === null) {
 					$('.display').append(`<button class="show-more p-2" type="button" data-genre='popular'><span>Show more</span></button>`);
-					
-					
-				} else{
+
+
+				} else {
 					$('.display').append(`<button class="show-more" type="button" data-genre=${y}><span>Show more</span></button>`);
-					
+
 				}
-
-					
-
-			
-
 			})
 			.catch(err => {
 				console.error(err);
 			});
 	}
- 
+
 	//Discover function - display random/popular movies
 
-	let discoverMovies = function () {
+	let discoverMovies = () => {
 
 		let url = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
 		runSearch(url, null);
-
 	}
 
-	let clear = function () {
+	let clear = () => {
 		$('.display').empty();
 	}
 
-	
-	
+	//Fill start page 
+	discoverMovies();
 
-	discoverMovies(); //fill start page 
 
-	
 	//Display movies depends on the genre (data-id)
-	$('a[data-id]').click(function () {	
+	$('a[data-id]').click(function () {
 		$('a').removeClass('active');
 		$(this).addClass('active');
-		if($(this).attr('data-id') === "popular"){
+		if ($(this).attr('data-id') === "popular") {
 			$('.display').show();
 			$('.display2').hide();
-			pageCount = 1;
 			clear();
 			discoverMovies();
-		}else{
+		} else {
 			$('.display').show();
 			$('.display-2').hide();
-			pageCount = 1;
-			let x = $(this).attr('data-id');
-			let y = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${pageCount}&with_genres=${x}`;
+			let genre = $(this).attr('data-id');
+			let url = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${pageCount}&with_genres=${genre}`;
 			clear();
-			runSearch(y, x);
+			runSearch(url, genre);
 		}
-
-		
 	})
 
 	//Show-more button => append movies
 	$(document).on('click', '.show-more', function () {
 
 		pageCount++;
-		var g = $(this).attr("data-genre");
-		var s = $(this).attr("data-search");
-		console.log(s);
-		if(s){
+		let genre = $(this).attr("data-genre");
+		let search = $(this).attr("data-search");
 
-			console.log("dzialam");
-			url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query='${s}'&page=${pageCount}`;
+		if (search) {
+
+			url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query='${search}'&page=${pageCount}`;
 			$(this).remove();
-			runSearch(url,0, s);
-		}
-		else if (g === 'popular') {
+			runSearch(url, 0, s);
+		} else if (genre === 'popular') {
 			url = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${pageCount}`;
 
 
 			$(this).remove();
 			runSearch(url, null);
-		} else if (g){
-			url = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${pageCount}&with_genres=${g}`;
+		} else if (genre) {
+			url = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${pageCount}&with_genres=${genre}`;
 			$(this).remove();
-			runSearch(url, g);
+			runSearch(url, genre);
 		}
 
 	});
 
 	//Search input 
-	$("form").submit(function(e) {
+	$("form").submit(function (e) {
 		e.preventDefault();
 		$('.display').show();
 		$('.display-2').hide();
 		let search = $('.search-input').val();
 		console.log(search);
-		url= `https://api.themoviedb.org/3/search/movie?api_key=${key}&query='${search}'&page=1`;
+		url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query='${search}'&page=1`;
 		clear();
-		runSearch(url,0,search);
+		runSearch(url, 0, search);
 	});
 
 
-$(document).on('click','.back',function(){
-	$('.display').show();
-	$('.display-2').hide();
-})
+	$(document).on('click', '.back', function () {
+		$('.display').show();
+		$('.display-2').hide();
+	})
 
 	$(document).on('click', '.movie-container', function () {
-		var o = $(this).attr('data-id');
-		console.log(o);
-		url = `https://api.themoviedb.org/3/movie/${o}?api_key=${key}&language=en-US`;
+		let filmId = $(this).attr('data-id');
+		console.log(filmId);
+		url = `https://api.themoviedb.org/3/movie/${filmId}?api_key=${key}&language=en-US`;
 		fetch(url)
 			.then((response) => {
 				return response.json();
@@ -172,7 +150,7 @@ $(document).on('click','.back',function(){
 				console.log(data);
 				$('.display').hide();
 				$('.display-2').show();
-				
+
 				$('html,body').animate({
 					scrollTop: $("#ds-2").offset().top
 				}, );
@@ -187,16 +165,16 @@ $(document).on('click','.back',function(){
 						<p>${data.overview}</p>
 					</div>`
 				);
-	})
+			})
 	});
 
 	////////////////////////////////////////////
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
 
 
 });
